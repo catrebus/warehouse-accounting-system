6f6e1355f7e95b03924242637159458b3534ec9b
catrebus <catrebus@mail.ru>
2025-11-05 21:38:33 +0300

    finish shipments window ui

--- services/shipments_service.py
+++ services/shipments_service.py
@@ -1,13 +1,50 @@
-from sqlalchemy import select
+from ctypes.wintypes import HDESK
+
+from sqlalchemy import select, func
 
 from db.db_session import get_db_session
-from db.models import Supplier
+from db.models import Supplier, Shipment, Employee, Warehouse
 
 
-def get_suppliers():
+def get_shipments_data():
+    with get_db_session() as session:
+        try:
+            stmt = select(Shipment.id, Supplier.name, func.concat(Employee.last_name, ' ' , Employee.first_name), Warehouse.name, Shipment.date)\
+                .join(Supplier, Supplier.id == Shipment.supplier_id)\
+                .join(Employee, Employee.id == Shipment.employee_id)\
+                .join(Warehouse, Warehouse.id == Shipment.warehouse_id)\
+                .order_by(Shipment.date.desc())
+            shipments = session.execute(stmt).all()
+            return {
+                'success': True,
+                'data': shipments
+            }
+        except Exception as e:
+            return {
+                'success': False,
+                'data': str(e)
+            }
+
+
+def get_suppliers_name():
     with get_db_session() as session:
         try:
             stmt = select(Supplier.id, Supplier.name)
+            suppliers = session.execute(stmt).all()
+            return {
+                'success': True,
+                'data': suppliers
+            }
+        except Exception as e:
+            return {
+                'success': False,
+                'data': str(e)
+            }
+
+def get_suppliers_data():
+    with get_db_session() as session:
+        try:
+            stmt = select(Supplier.name, Supplier.phone_number, Supplier.email)
             suppliers = session.execute(stmt).all()
             return {
                 'success': True,
--- ui/main_windows/shipments_window.py
+++ ui/main_windows/shipments_window.py
@@ -1,11 +1,13 @@
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QColor
+from PyQt6.QtCore import QSize, Qt, QRegularExpression
+from PyQt6.QtGui import QColor, QRegularExpressionValidator
 from PyQt6.QtWidgets import QLabel, QWidget, QWIDGETSIZE_MAX, QVBoxLayout, QStackedLayout, QFrame, \
-    QGraphicsDropShadowEffect, QHBoxLayout, QComboBox, QPushButton, QMessageBox, QScrollArea
+    QGraphicsDropShadowEffect, QHBoxLayout, QComboBox, QPushButton, QMessageBox, QScrollArea, QTableView, QHeaderView, \
+    QLineEdit
 
-from services.shipments_service import get_suppliers
+from services.shipments_service import get_suppliers_name, get_suppliers_data, get_shipments_data
 from ui.base_window import BaseWindow
 from ui.ui_elements.nav_panel import NavPanel
+from ui.ui_elements.table_model import TableModel
 from utils.app_state import AppState
 
 
@@ -58,7 +60,7 @@ class ShipmentsWindow(BaseWindow):
         # Подложка под элементы
         self.newShipmentCard = QFrame()
         self.newShipmentCard.setObjectName('cardLogin')
-        self.newShipmentCard.setFixedSize(1000, 150)
+        self.newShipmentCard.setFixedSize(1000, 400)
 
         newShipmentLayout = QVBoxLayout(self.newShipmentCard)
         newShipmentLayout.setContentsMargins(15, 15, 15, 15)
@@ -69,6 +71,27 @@ class ShipmentsWindow(BaseWindow):
         cardEffect.setYOffset(4)
         cardEffect.setColor(QColor(0, 0, 0, 120))
         self.newShipmentCard.setGraphicsEffect(cardEffect)
+
+        # Заголовок таблицы принятых поставок
+        shipmentTableLabel = QLabel('Принятые поставки')
+        newShipmentLayout.addWidget(shipmentTableLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+        newShipmentLayout.addSpacing(10)
+
+        shipmentsHeaders = ['Id', 'Поставщик', 'Сотрудник', 'Склад', 'Дата']
+        shipmentsData = get_shipments_data()['data']
+        self.suppliersModel = TableModel(shipmentsData, shipmentsHeaders)
+
+        # Таблица поставок
+        shipmentsTable = QTableView()
+        shipmentsTable.verticalHeader().setVisible(False)
+        shipmentsTable.setModel(self.suppliersModel)
+        shipmentsTable.resizeColumnsToContents()
+        shipmentsTable.setAlternatingRowColors(True)
+        shipmentsTable.setSelectionBehavior(shipmentsTable.SelectionBehavior.SelectRows)
+        shipmentsTable.setFixedSize(700, 200)
+        shipmentsTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        newShipmentLayout.addWidget(shipmentsTable, alignment=Qt.AlignmentFlag.AlignCenter)
+        newShipmentLayout.addSpacing(10)
 
         # Заголовок элементов создания поставки
         newShipmentLabel = QLabel('Создание поставки')
@@ -103,7 +126,7 @@ class ShipmentsWindow(BaseWindow):
         if self.user.role in [1,3]:
             self.newSupplierCard = QFrame()
             self.newSupplierCard.setObjectName('cardLogin')
-            self.newSupplierCard.setFixedSize(1000, 150)
+            self.newSupplierCard.setFixedSize(1000, 420)
 
             newSupplierLayout = QVBoxLayout(self.newSupplierCard)
             newSupplierLayout.setContentsMargins(15, 15, 15, 15)
@@ -116,9 +139,81 @@ class ShipmentsWindow(BaseWindow):
             self.newSupplierCard.setGraphicsEffect(cardEffect)
 
             # Заголовок элементов для добавления поставщика
-            newSupplierLabel = QLabel('Добавление поставщика')
+            newSupplierLabel = QLabel('Поставщики')
             newSupplierLayout.addWidget(newSupplierLabel, alignment=Qt.AlignmentFlag.AlignCenter)
             newSupplierLayout.addSpacing(10)
+
+            # Модель для таблицы поставщиков
+            suppliersHeaders = ['Имя', 'Номер телефона', 'Почта']
+            suppliersData = get_suppliers_data()['data']
+            self.suppliersModel = TableModel(suppliersData, suppliersHeaders)
+
+            # Таблица для поставщиков
+            suppliersTable = QTableView()
+            suppliersTable.verticalHeader().setVisible(False)
+            suppliersTable.setModel(self.suppliersModel)
+            suppliersTable.resizeColumnsToContents()
+            suppliersTable.setAlternatingRowColors(True)
+            suppliersTable.setSelectionBehavior(suppliersTable.SelectionBehavior.SelectRows)
+            suppliersTable.setFixedSize(700, 200)
+            suppliersTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+            newSupplierLayout.addWidget(suppliersTable, alignment=Qt.AlignmentFlag.AlignCenter)
+
+            suppliersTableLabel = QLabel('Добавление нового поставщика')
+            newSupplierLayout.addWidget(suppliersTableLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+
+
+            # Поля ввода данных о новом поставщике
+            newSupplierNameLayout = QHBoxLayout() # Имя поставщика
+            newSupplierNameLayout.addStretch()
+
+            newSupplierNameLabel = QLabel('Имя: ')
+            newSupplierNameLabel.setFixedWidth(150)
+            newSupplierNameLine = QLineEdit()
+            newSupplierNameLine.setFixedWidth(250)
+            newSupplierNameLine.setPlaceholderText('Имя нового поставщика')
+            newSupplierNameLine.setMaxLength(45)
+
+            newSupplierNameLayout.addWidget(newSupplierNameLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+            newSupplierNameLayout.addWidget(newSupplierNameLine, alignment=Qt.AlignmentFlag.AlignCenter)
+            newSupplierNameLayout.addStretch()
+            newSupplierLayout.addLayout(newSupplierNameLayout)
+
+            newSupplierPhoneLayout = QHBoxLayout() # Номер телефона поставщика
+            newSupplierPhoneLayout.addStretch()
+
+            newSupplierPhoneLabel = QLabel('Номер телефона: ')
+            newSupplierPhoneLabel.setFixedWidth(150)
+            newSupplierPhoneLine = QLineEdit()
+            newSupplierPhoneLine.setFixedWidth(250)
+            newSupplierPhoneLine.setPlaceholderText('Телефон нового поставщика')
+            newSupplierPhoneLine.setMaxLength(20)
+            newSupplierPhoneLine.setValidator(QRegularExpressionValidator(QRegularExpression(r'^\d*$')))
+
+            newSupplierPhoneLayout.addWidget(newSupplierPhoneLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+            newSupplierPhoneLayout.addWidget(newSupplierPhoneLine, alignment=Qt.AlignmentFlag.AlignCenter)
+            newSupplierPhoneLayout.addStretch()
+            newSupplierLayout.addLayout(newSupplierPhoneLayout)
+
+            newSupplierEmailLayout = QHBoxLayout() # Email поставщика
+            newSupplierEmailLayout.addStretch()
+
+            newSupplierEmailLabel = QLabel('Почта: ')
+            newSupplierEmailLabel.setFixedWidth(150)
+            newSupplierEmailLine = QLineEdit()
+            newSupplierEmailLine.setFixedWidth(250)
+            newSupplierEmailLine.setPlaceholderText('Электронная почта')
+            newSupplierEmailLine.setMaxLength(45)
+
+            newSupplierEmailLayout.addWidget(newSupplierEmailLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+            newSupplierEmailLayout.addWidget(newSupplierEmailLine, alignment=Qt.AlignmentFlag.AlignCenter)
+            newSupplierEmailLayout.addStretch()
+            newSupplierLayout.addLayout(newSupplierEmailLayout)
+
+            addNewSupplierBtn = QPushButton('Добавить')
+            addNewSupplierBtn.clicked.connect(self.handle_add_new_supplier)
+            addNewSupplierBtn.setFixedWidth(200)
+            newSupplierLayout.addWidget(addNewSupplierBtn, alignment=Qt.AlignmentFlag.AlignCenter)
 
             newSupplierLayout.addStretch()
             contentLayout.addWidget(self.newSupplierCard, alignment=Qt.AlignmentFlag.AlignCenter)
@@ -137,7 +232,7 @@ class ShipmentsWindow(BaseWindow):
         """Загрузка поставщиков из бд в список для создания поставки"""
         self.supplierSelection.clear()
         self.supplierSelection.addItem('Выберите поставщика')
-        suppliers = get_suppliers()
+        suppliers = get_suppliers_name()
         if suppliers['success']:
             for supplierId, supplierName in suppliers['data']:
                 self.supplierSelection.addItem(supplierName, supplierId)
@@ -148,3 +243,11 @@ class ShipmentsWindow(BaseWindow):
     def handle_create_new_shipment(self):
         """Обработка нажатия на кнопку создания поставки"""
         pass
+
+    def handle_add_new_supplier(self):
+        pass
+
+    def update_suppliers_table(self):
+        """Обновление таблицы с поставщиками"""
+        newSuppliersData = get_suppliers_data()['data']
+        self.suppliersModel.update_data(newSuppliersData)
